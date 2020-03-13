017ea86f8133cda95bf647098bf56289dcc11e50
Vlad Spirin <vladspirin@hotmail.com>
2020-03-13 18:12:42 +0200

    Next lesson, operators

---
+++ self-learning/based/ifoperator.py
@@ -0,0 +1,59 @@
+# True
+# False
+
+# x = 0
+# if x:
+#     print('Переменная x вернула истину')
+# else:
+#     print('Переменная x вернула ложь')
+
+# if 1:
+#     print('Выражение истинно')
+# else:
+#     print('Выражение ложно')
+
+# light = 'red'
+# if light == 'red':
+#     print("Stop")
+# elif light == 'yellow':
+#     print('Wait')
+# elif light == 'green':
+#     print('Go')
+# else:
+#     print('What?')
+
+# age = int(input('Сколько Вам лет? '))
+#
+# if age >= 18:
+#     print('Добро пожаловать!')
+# else:
+#     print(f'Вам {age} лет, не хватает {18 - age}')
+
+# operator or
+# time = 11
+# if time < 12 or time > 13:
+#     print('Open')
+# else:
+#     print('Close')
+
+# operator and
+# time = 8
+# day = 'Saturday'
+#
+# if time >= 8 and day != 'Sunday':
+#     print('Open')
+# else:
+#     print('Close')
+
+# инвертирование выражения
+# x = 1
+# if not x:
+#     print('OK')
+# else:
+#     print('NO')
+
+# подобие тернарного оператора, в Python его нет
+x = 0
+# result = 'OK' if x else 'NO'
+# print(result)
+print('OK' if x else 'NO')
