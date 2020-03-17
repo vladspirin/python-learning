03b76c263677db5eb6aaa4db49f96cb2d7a8dec1
Vlad Spirin <vladspirin@hotmail.com>
2020-03-17 19:37:25 +0200

    Add next lessons

---
+++ self-learning/based/loops.py
@@ -0,0 +1,32 @@
+# while stmt:
+#     do...
+# while True:
+#     print('Hello')
+# i = 1
+# while i <= 10:
+#     print(i, end=' ')
+#     # i = i + 1
+#     i += 1
+
+
+# for
+# s = 'Hello world'
+# for l in s:
+#     if l == ' ':
+#         continue
+#     print(f'"{l}"', end=' ')
+# else (for)
+# for i in 'Helloworld':
+#     if i == ' ':
+#         break
+#     print(i, end=' ')
+# else:
+#     print('\nNo spaces')
+# HW
+cal = 1900
+
+while cal <= 2020:
+    print(cal)
+    cal += 1
+else:
+    print('Done')
