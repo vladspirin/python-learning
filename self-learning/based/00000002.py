ba2d28e8be00cccea9cd40148b43c1e9fb930ef3
Vladyslav Spirin <vladspirin@hotmail.com>
2020-06-10 23:10:46 +0300

    The problem of the day solution, and smth else

--- self-learning/based/00000002.py
+++ self-learning/based/00000002.py
@@ -1,3 +1,13 @@
-args = ["script.py", "1", "2", "3", "4"]
-my_list = [int(x) for i, x in enumerate(args) if i > 0]
-print(str(my_list))
+# args = ["script.py", "1", "2", "3", "4"]
+# my_list = [int(x) for i, x in enumerate(args) if i > 0]
+# print(str(my_list))
+
+vowels = 'aeiou'
+text = input()
+for i in text:
+    if i in vowels:
+        print("vowel")
+    elif i not in vowels and i.isalpha():
+        print("consonant")
+    elif not i.isalpha():
+        break    
