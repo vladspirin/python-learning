b48df8e4fe77a3220c2e098fcf5037ed683b084f
Vladyslav Spirin <24988700+vladspirin@users.noreply.github.com>
2020-06-30 12:12:27 +0300

    Task with generators

---
+++ self-learning/based/00000010.py
@@ -0,0 +1,31 @@
+# 2.
+print("".join([chr(ord(i) + 1) for i in input()]))
+
+# 3.
+u_code = int(input())
+print(chr(u_code)) if u_code in range(32, 127) else print(False)
+
+# 4.
+numbers = [1, 2, 3]
+my_generator = (n ** 2 for n in numbers)
+for n in my_generator:
+    print(n)
+
+# 5.
+sum_gen = sum((int(i) for i in input()))
+
+# 6.
+
+n = int(input())
+
+
+def squares(num):
+    i = 1
+    while i <= num:
+        yield i ** 2
+        i += 1
+    
+
+my_generator = squares(n)
+for j in my_generator:
+    print(j)
